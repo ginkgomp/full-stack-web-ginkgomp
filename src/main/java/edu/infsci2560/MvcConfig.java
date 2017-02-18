e0eb56d60196002c4e635fe1522140f9476446d7
ginkgomp <lvyuopo@hotmail.com>
2017-02-18 16:15:54 -0500

    Deploy again

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -21,7 +21,9 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        
     }
 
 }
