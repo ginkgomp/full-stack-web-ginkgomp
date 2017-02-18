e9cfbfe3ec456a08b783068f3f08a3e899434735
ginkgomp <lvyuopo@hotmail.com>
2017-02-18 16:19:40 -0500

    Deploy again

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -2,6 +2,7 @@ package edu.infsci2560;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
