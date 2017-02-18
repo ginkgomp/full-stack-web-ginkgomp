d11a097be5f4c3657feff3be919bc86d71865b5e
ginkgomp <lvyuopo@hotmail.com>
2017-02-18 18:06:04 -0500

    Try

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -22,7 +22,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webjars/**").addResourceLocations("/classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/classpath:/META-INF/resources/static/webjars/");
         
     }
 
