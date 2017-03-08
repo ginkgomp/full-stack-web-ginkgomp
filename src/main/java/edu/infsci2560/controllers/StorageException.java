03e2958d74a90db891eff5e50a60fe2e9c016244
ginkgomp <lvyuopo@hotmail.com>
2017-03-08 16:20:48 -0600

    Add upload feature

---
+++ src/main/java/edu/infsci2560/controllers/StorageException.java
@@ -0,0 +1,13 @@
+package edu.infsci2560.controllers;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
