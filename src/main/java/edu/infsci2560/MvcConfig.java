bf3c4f187cf568784385d3de03b6ef517f6c0105
ginkgomp <lvyuopo@hotmail.com>
2017-03-10 00:22:14 -0600

    ..

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -1,7 +1,7 @@
 package edu.infsci2560;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.multipart.MultipartResolver;
