fe80720050f09540c118ba6300ca350282eb410a
ginkgomp <lvyuopo@hotmail.com>
2017-03-15 00:29:13 -0400

    Change

--- src/main/java/edu/infsci2560/WebSecurityConfig.java
+++ src/main/java/edu/infsci2560/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/","/home","/blogs","/ideas").permitAll()
+                .antMatchers("/","/home","/blogs","/locations").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
