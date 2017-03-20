ec1c929f954c5ce315abe5dc52965298bc890878
ginkgomp <lvyuopo@hotmail.com>
2017-03-20 04:24:15 -0400

    Try again

--- src/main/java/edu/infsci2560/controllers/HomeController.java
+++ src/main/java/edu/infsci2560/controllers/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Principal principal) {
-        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+        return principal != null ? "/home" : "/home";
     }
 }
