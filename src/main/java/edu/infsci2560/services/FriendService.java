fa806907f66cba43b8a1070ebd0f5d9bb41e288d
ginkgomp <lvyuopo@hotmail.com>
2017-03-26 15:51:09 -0400

    Try delete method

--- src/main/java/edu/infsci2560/services/FriendService.java
+++ src/main/java/edu/infsci2560/services/FriendService.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author Wenwen Sun
  */
 @RestController
-@RequestMapping("/public/api/friends")
+@RequestMapping("/friends")
 public class FriendService {
 
     @Autowired
@@ -49,5 +49,16 @@ public class FriendService {
     public ResponseEntity<Friend> create(@RequestBody Friend friends) {
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<>(repository.save(friends), headers, HttpStatus.OK);
+    }
+    
+    @RequestMapping(method=RequestMethod.DELETE, value="/{id}")
+    public void delete(@PathVariable Long id) {
+    	repository.delete(id);
+
+    }
+    
+    @RequestMapping(method=RequestMethod.PUT, value="/{id}")
+    public Friend update(@PathVariable Long id, @RequestBody Friend friend) {
+    return null;
     }
 }
