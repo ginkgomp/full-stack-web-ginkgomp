05f0b5a4ab1f329d5cf46b251950058c75d6e703
ginkgomp <lvyuopo@hotmail.com>
2017-03-28 01:56:59 -0400

    ..

--- src/main/java/edu/infsci2560/controllers/LocationEditController.java
+++ src/main/java/edu/infsci2560/controllers/LocationEditController.java
@@ -2,11 +2,13 @@ package edu.infsci2560.controllers;
 
 import java.io.IOException;
 
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
