9441ff4b1dc5852ce04ecf7db4d163807ede3552
ginkgomp <lvyuopo@hotmail.com>
2017-03-29 01:29:26 -0400

    Add security page

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -23,7 +23,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/").setViewName("security");
         registry.addViewController("/locations").setViewName("locations");
         registry.addViewController("/blogs").setViewName("blogs");
         registry.addViewController("/photos").setViewName("photos");
